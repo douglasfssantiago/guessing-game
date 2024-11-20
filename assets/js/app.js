050055cf41856215e150105d6a6aa08de638576e
Douglas Santiago <douglasfssantiago@gmail.com>
2024-11-20 11:48:16 -0600

    implementing code

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,9 @@ const counter = select('.counter p');
 const inputText = select('.input-text');
 const playAgainBtn = select('#play-again-button');
 
-let score = 0; 
-
+let randomNumber = generateRandomNumber(); 
+let attemptsLeft = 10; 
+counter.textContent = `Guesses: ${attemptsLeft}`;
 
 function select(selector, scope = document) {
     return scope.querySelector(selector);
@@ -17,12 +18,15 @@ function listen(event, selector, callback) {
     return selector.addEventListener(event, callback);
 }
 
+function generateRandomNumber() { 
+    return Math.floor(Math.random() * 1000); }
+
 function validateInput(input) {
     input = input.trim();
     if (/^\d{1,3}$/.test(input) && Number(input) >= 0 && Number(input) <= 999) {
         return true; 
     }
-    resultDisplay.textContent = 'Insert a valid number (0-999)!';
+    resultDisplay.textContent = 'Insert a number (0-999)!';
     return false;  
 }
 
@@ -35,6 +39,34 @@ inputText.addEventListener('input', function() {
         } 
     });
 
+function checkGuess() {
+    const userGuess = Number(inputText.value);
+
+    if (validateInput(inputText.value)) {
+        if (userGuess === randomNumber) {
+            resultDisplay.textContent = 'You win!';
+            inputText.disabled = true;
+        } else {
+            attemptsLeft--;
+            counter.textContent = `Guesses: ${attemptsLeft}`;
+            if (attemptsLeft <= 0) {
+                resultDisplay.textContent = 'Game over!';
+                inputText.disabled = true;
+            } else if (userGuess < randomNumber) {
+                resultDisplay.textContent = 'My number is bigger!';
+            } else {
+                resultDisplay.textContent = 'My number is smaller!';
+            }
+        }
+        inputText.value = '';
+    }
+}
+
+inputText.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        checkGuess();
+    }
+});
 
 
 
